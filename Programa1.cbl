6d4f5f2ba9731df84d97a92f52bf77aa243d6a98
Pablomp94 <1744623@alu.murciaeduca.es>
2024-11-07 13:17:26 +0100

    COBOL

--- Programa1.cbl
+++ Programa1.cbl
@@ -21,11 +21,24 @@
            DISPLAY RESULTADO.
 
        AÑADEVALORAVARIABLE.
+           *>Añade el valor de Numero2 a Numero1
            ADD Numero2 to Numero1.
        MUESTRA.
            DISPLAY Numero1.
 
+       CALCULADORA.
 
+           DISPLAY "Introduce un numero:".
+
+           ACCEPT Numero1.
+
+           DISPLAY "Introduce otro numero:".
+
+           ACCEPT Numero2.
+
+           COMPUTE RESULTADO = Numero1 + Numero2.
+
+           DISPLAY "La suma de ambos numeros es: " RESULTADO.
 
             STOP RUN.
        END PROGRAM Programa1.
